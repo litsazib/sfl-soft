46489a1c4433da38f2d1424cfa0aaec608c9e3f8
litsazib <roktimsazib92@gmail.com>
2020-09-12 00:17:19 +0530

    update Dashbord

--- vg_view/Dashboard/Dashboard.php
+++ vg_view/Dashboard/Dashboard.php
@@ -2,12 +2,8 @@
 
 <div class="mainwrapper">
 	<?php require_once(View."menu.php");?>
-
-   
-   
         <div class="breadcrumbwidget">
         	<ul class="skins">
-                
                 <li class="fixed"><a href="#" class="skin-layout fixed"></a></li>
                 <li class="wide"><a href="#" class="skin-layout wide"></a></li>
             </ul><!--skins-->
@@ -22,52 +18,38 @@
        
       <div class="contentinner content-dashboard">
             	
-                <?php
-                
+        <?php    
 				$totalUser=$this->sql("SELECT COUNT(`id_employee`) AS 't_user' FROM `vg_employee`");
 				$vg_supplier=$this->sql("SELECT COUNT(`id_supplier`) AS 'vg_supplier' FROM `vg_supplier`");
 				$customer=$this->sql("SELECT COUNT(`customer_id`) AS 'customer' FROM `customer`");
 				$Purchase=$this->sql("SELECT COUNT(`id`) AS 't_p_inv' FROM `supplier_accounts` WHERE `reperence_status`='Purchase'");
 				$Sales=$this->sql("SELECT COUNT(`id`) AS 't_s_inv' FROM `customer_accounts` WHERE `reperence_status`='Sales'");
-				
-				
 				?>
                 
-                <div class="row-fluid">
-                	<div class="span11">
-                    	<ul class="widgeticons row-fluid">
-                        	<?php /*?><li class="one_fifth"><a href="#"><img src="img/gemicon/users.png" alt="" /><span>User: <?php echo $totalUser[0]['t_user']?></span></a></li><?php */?>
-                            <li class="one_fifth"><a href="./?Supplier_AllSupplier"><img src="img/gemicon/users.png" alt="" /><span>Supplier: <?php echo $vg_supplier[0]['vg_supplier']?></span></a></li>
-                            <li class="one_fifth"><a href="./?Customer_AllCustomer"><img src="img/gemicon/users.png" alt="" /><span>Customer: <?php echo $customer[0]['customer']?></span></a></li>
-                            
-                            <li class="one_fifth last"><a href="#"><img src="img/gemicon/purchase.png" alt="" /><span>P.INV: <?php echo $Purchase[0]['t_p_inv']?></span></a></li>
-                        	<li class="one_fifth last"><a href="#"><img src="img/gemicon/sales.png" alt="" /><span>S.INV: <?php echo $Sales[0]['t_s_inv']?></span></a></li>
-                        	
-                        </ul>
-                        
-                     
-                      <!--  <br />
-                        
-                        <h4 class="widgettitle">Report Summary</h4>
-                        <div class="widgetcontent">
-                        	<div id="chartplace2" style="height:300px;"></div>
-                        </div><!--widgetcontent-->
-                      
-                        
-                    </div><!--span11-->
-                    
-                    
-                    
-                </div><!--row-fluid-->
-            </div>
-       
-        <!--maincontent-->
-        
-    </div><!--mainright-->
-    <!-- END OF RIGHT PANEL -->
- 	
-  
-    
+        <div class="row-fluid">
+      	   <div class="span11">
+        	 <ul class="widgeticons row-fluid">
+            	<?php?><li class="one_fifth"><a href="#"><img width="70%;" src="img/gemicon/userIcon.webp" alt="" /><span>User: <?php echo $totalUser[0]['t_user']?></span></a></li><?php?>
+              
+                <li class="one_fifth"><a href="./?Supplier_AllSupplier"><img width="70%" src="img/gemicon/supplayer.png" alt="" /><span>Supplier: <?php echo $vg_supplier[0]['vg_supplier']?></span></a></li>
+                
+                <li class="one_fifth"><a href="./?Customer_AllCustomer"><img width="70px;" src="img/gemicon/customer.jpg" alt="" /><span>Customer: <?php echo $customer[0]['customer']?></span></a></li>
+                
+                <li class="one_fifth last"><a href="#"><img src="img/gemicon/purchase.png" alt="" /><span>P.INV: <?php echo $Purchase[0]['t_p_inv']?></span></a></li>
+                
+            	<li class="one_fifth last"><a href="#"><img src="img/gemicon/sales.png" alt="" /><span>S.INV: <?php echo $Sales[0]['t_s_inv']?></span></a></li>
+              
+            </ul>
+                
+              
+                
+            </div><!--span11-->
+        </div>
+         <h1 class="dashboardTitle">SOLAIMAN FEEDS LTD</h1>
+         <h5 class="subT">Stock Managment Sulation</h5>
+        </div>
+
+    </div>
     <script type="text/javascript">
 jQuery(document).ready(function(){
 								
