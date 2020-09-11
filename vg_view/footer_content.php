3c16f08cac6eba88ffe7f0c08195d6c81cff3492
litsazib <roktimsazib92@gmail.com>
2020-09-11 23:27:46 +0530

    set HotLinke Number :wq

--- vg_view/footer_content.php
+++ vg_view/footer_content.php
@@ -1,6 +1,6 @@
  <div class="clearfix"></div>
     
     <div class="footer">
-    	<div class="footerleft">Powered by:CodingAvatar Inc</div>
-    	<div class="footerright">&copy; - <a href="http://www.codingavatar.com/">Follow me on CodingAvatar Inc</a></div>
+    	<div class="footerleft">Powered By: <a href="http://codingavatar.com/">Coding Avatar Inc. </a>HotLine:01724255853</div>
+    	<div class="footerright">&copy; - <a href="https://www.facebook.com/sazib.pip">Follow me on Roktim Sazib</a></div>
     </div><!--footer-->
