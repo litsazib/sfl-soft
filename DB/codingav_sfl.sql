2112b419ab65da3c93439b3671be2c81757c45c3
litsazib <roktimsazib92@gmail.com>
2020-09-12 14:34:05 +0530

    add git ignor fifle

--- DB/codingav_sfl.sql
+++ DB/codingav_sfl.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `codingav_sfl`
+-- Database: `solaiman_soft`
 --
 
 -- --------------------------------------------------------
